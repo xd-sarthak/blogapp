5784156734d678ded940b8499ffdd73a90c4e1ce
xd-sarthak <sarthak.sri02032gmail.com>
2025-11-09 20:22:02 +0530

    comments added

--- services/blog/src/controllers/blog.controller.ts
+++ services/blog/src/controllers/blog.controller.ts
@@ -3,6 +3,7 @@ import { sql } from "../utils/db.js";
 import { Request, Response } from "express";
 import axios from "axios";
 import { redisClient } from "../server.js";
+import { AuthenticatedRequest } from "../middlewares/auth.middleware.js";
 
 export const getBlogs = TryCatch(async (req: Request, res: Response) => {
   const { searchQuery = "", category = "" } = req.query;
@@ -79,3 +80,78 @@ export const getSingleBlog = TryCatch(async (req: Request, res: Response) => {
     })
    
 });
+
+export const addComment = TryCatch(async(req: AuthenticatedRequest, res:Response) => {
+    const {id:blogid} = req.params;
+    const {comment} = req.body;
+
+    await sql `INSERT INTO comments (comment, blogid, userid, username) VALUES (${comment}, ${blogid}, ${req.user?._id}, ${req.user?.name}) RETURNING *`;
+
+    res.json({
+      message: "Comment Added"
+    });
+});
+
+export const getAllComment = TryCatch(async (req:Request,res:Response) => {
+  const {id} = req.params;
+  const comments = await sql `SELECT * FROM comments WHERE blogid = ${id} ORDER BY create_at DESC`;
+
+  res.json({
+    comments
+  });
+});
+
+export const deleteComment = TryCatch(async (req:AuthenticatedRequest,res:Response) => {
+  const {commentid} = req.params;
+  const comment = await sql `SELECT * FROM comments WHERE id = ${commentid}`;
+  console.log(comment);
+
+  if(comment[0].userid != req.user?._id){
+    res.status(401).json({
+      message: "you are not authorised to delete"
+    });
+    return;
+  }
+
+  await sql `DELETE FROM comments WHERE id = ${commentid}`;
+
+  res.json({
+    message: "Comment Deleted"
+  });
+});
+
+export const saveBlogs = TryCatch(async(req:AuthenticatedRequest,res:Response) => {
+  const {blogid} = req.params;
+  const userid = req.user?._id;
+
+  if(!blogid || !userid){
+    res.status(400).json({
+      message: "Blogid or Userid missing"
+    });
+    return;
+  }
+
+  const existing = await sql `SELECT * FROM savedblogs WHERE userid = ${userid} AND blogid = ${blogid}`;
+
+  if(existing.length === 0){
+    await sql `INSERT INTO savedblogs (blogid, userid) VALUES (${blogid}, ${userid})`;
+    res.json({
+      message: "Blog Saved"
+    });
+    return;
+  }
+  else{
+    await sql `DELETE FROM savedblogs WHERE userid = ${userid} AND blogid = ${blogid}`;
+    res.json({
+      message: "Blog Saved"
+    });
+    return;
+  }
+});
+
+export const getSavedBlog = TryCatch(async(req:AuthenticatedRequest,res:Response) => {
+  const blogid = req.user?._id;
+  const blogs = await sql `SELECT * FROM savedblogs WHERE userid = ${blogid}`;
+
+  res.json(blogs);
+})
--- services/blog/src/routes/blog.route.ts
+++ services/blog/src/routes/blog.route.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { getBlogs,getSingleBlog } from "../controllers/blog.controller.js";
+import { addComment, getAllComment, getBlogs,getSavedBlog,getSingleBlog, saveBlogs } from "../controllers/blog.controller.js";
+import { isAuth } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/blogs/all",getBlogs);
 router.get("/blogs/:id",getSingleBlog);
+router.post("/comment/:id",isAuth,addComment);
+router.get("/comment/:id",getAllComment);
+router.delete("/comment/:commentid",isAuth,addComment);
+router.post("/save/:blogid",isAuth,saveBlogs);
+router.get("/blog/saved/all",isAuth,getSavedBlog);
 
 export default router;
